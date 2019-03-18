1f05b56e0fa03455a737c78794d5839ceb9512ac
= <=>
2019-03-19 00:27:54 +0300

    section 7

---
+++ ZwajApp-SPA/src/app/_guards/auth.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../_services/Auth.service';
+import { AlertifyService } from '../_services/alertify.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService:AuthService,private router:Router,private alertify:AlertifyService) {}
+  canActivate():  boolean {
+    if(this.authService.loggedIn()){
+      return true;
+
+    }
+    this.alertify.error('يجب عليك تسجيل الدخول أولا');
+    this.router.navigate(['']);
+    return false;
+  }
+}
--- ZwajApp-SPA/src/app/nav/nav.component.ts
+++ ZwajApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/Auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import {  Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -9,7 +10,8 @@ import { AlertifyService } from '../_services/alertify.service';
 })
 export class NavComponent implements OnInit {
 model:any={};
-  constructor(public authService:AuthService , private alertify:AlertifyService) { }
+  constructor(public authService:AuthService , private alertify:AlertifyService,
+    private router:Router) { }
 
   ngOnInit() {
   }
@@ -18,6 +20,7 @@ model:any={};
     this.authService.login(this.model).subscribe(
       next=>{this.alertify.success("تم الدخول بنجاح")},
       error=>{this.alertify.error(error)},
+      ()=>this.router.navigate(['members'])
      
     )
   }
@@ -29,6 +32,7 @@ model:any={};
   loggedOut(){
     localStorage.removeItem('token');
     this.alertify.message('تم الخروج بنجاح');
+    this.router.navigate(['home']);
   }
 
 }
